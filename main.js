9d9e6feaae759e45fafc5500dece7de634627649
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-12-25 17:36:29 +1100

    display author data for returned book

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 async function searchByIsbn() {
     // clear results
-    document.getElementById('results-section').innerHTML = 'Loading...';
+    document.getElementById('book-results-section').innerHTML = 'Loading...';
+    document.getElementById('authors-results-section').innerHTML = '';
 
-    // grab inputs
+    // grab isbn input
     const isbn = document.getElementById('isbn').value;
 
-    // fetch the data
-    const response = await fetch('https://openlibrary.org/isbn/' + isbn + '.json', {
+    // fetch the book data
+    const bookResponse = await fetch('https://openlibrary.org/isbn/' + isbn + '.json', {
         method: 'GET'
     });
-    const json = await response.json();
-    const result = JSON.stringify(json,null,4);
+    const bookJson = await bookResponse.json();
+    const bookResult = JSON.stringify(bookJson,null,4);
+    const authorIdentifiers = [];
+    bookJson.authors.forEach((authorEntry) => {
+        authorIdentifiers.push(authorEntry.key);
+        console.log('got author key: ' + authorEntry.key);
+    });
+
+    // fetch the authors data
+    let authorsHtml = 'Authors: <pre><code class="prettyprint">';
+    for (const authorId of authorIdentifiers) {
+        authorsHtml += await fetchAndStringifyAuthor(authorId);
+    }
+    authorsHtml += '</code></pre>';
 
     // display the results
-    document.getElementById('results-section').innerHTML = '<pre><code class="prettyprint">' + result + '</code></pre>';
+    document.getElementById('book-results-section').innerHTML = 'Book: <pre><code class="prettyprint">' + bookResult + '</code></pre>';
+    document.getElementById('authors-results-section').innerHTML = authorsHtml;
+}
+
+async function fetchAndStringifyAuthor(key) {
+    const authorResponse = await fetch('https://openlibrary.org' + key + '.json', {
+        method: 'GET'
+    });
+    const authorJson = await authorResponse.json();
+
+    return JSON.stringify(authorJson,null,4);
 }
